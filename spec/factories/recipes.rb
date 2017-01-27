acb911a21575ba303c484adc229f17bd55645632
Fernanda Monteiro <fmlopes42@gmail.com>
2017-01-27 01:35:33 -0200

    WIP feature add recipes

---
+++ spec/factories/recipes.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :recipe do
+    name 'Miojo'
+    kitchen 'Italiana'
+    food_type 'Massa'
+    serves 1
+    prep_time 3
+    difficulty 1
+    ingredients '1 pct de miojo'
+    prep_steps 'Ferva água, coloque o miojo e aguarde 3 minutos.'
+  end
+end
---
+++ spec/features/user_register_recipes_spec.rb
@@ -0,0 +1,27 @@
+require 'rails-helper'
+
+feature 'User register recipes' do
+  scenario 'successfully' do
+    recipe = Recipe.new(attributes_for(:recipe)
+
+    visit new_recipe_path
+
+    fill_in 'Nome' with recipe.name
+    fill_in 'Tipo de comida' with recipe.food_type
+    fill_in 'Cozinha' with recipe.kitchen
+    fill_in 'Quantas pessoas serve' with recipe.serves
+    fill_in 'Tempo de preparo' with recipe.prep_time
+    select  recipe.difficulty from 'Nivel de dificuldade'
+    fill_in 'Ingredientes' with recipe.ingredients
+    fill_in 'Passo a passo' with prep_steps
+
+    expect(page).to have_content recipe.name
+    expect(page).to have_content recipe.food_type
+    expect(page).to have_content recipe.kitchen
+    expect(page).to have_content recipe.serves
+    expect(page).to have_content recipe.prep_time
+    expect(page).to have_content recipe.difficulty
+    expect(page).to have_content recipe.ingredients
+    expect(page).to have_content recipe.prep_steps
+  end
+end
